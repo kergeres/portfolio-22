1d14ff0d671763d2f5666b9b76326e787b6c8104
kergeres <55786735+kergeres@users.noreply.github.com>
2022-01-17 21:03:24 +0100

    hovers, cv, text anim

--- script/main.js
+++ script/main.js
@@ -1,16 +1,33 @@
 "use strict";
 
 
-
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 
 gtag('config', 'G-2E86DF0HZ1');
 
-
 document.querySelectorAll('.upper-container div, .lower-container div').forEach(pic => {
     pic.addEventListener('click', (e) => {
         window.open("https://photos.app.goo.gl/RTEVkvdS5fLeFDD37")
     })
 })
+
+function textAnimatoin() {
+
+    let top = document.querySelector(".text");
+    let introPosition = top.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight / 1.7;
+    if (introPosition < screenPosition) {
+        top.classList.add("text-show");
+    }
+
+}
+
+
+window.addEventListener("scroll", function () {
+    textAnimatoin();
+
+
+
+});
