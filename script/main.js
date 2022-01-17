08c21bac6ff571225978d199ec96ddc94d79c743
kergeres <55786735+kergeres@users.noreply.github.com>
2022-01-17 16:18:26 +0100

    google nltcs

--- script/main.js
+++ script/main.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log("cs");
+
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'G-2E86DF0HZ1');
+
+
+document.querySelectorAll('.upper-container div, .lower-container div').forEach(pic => {
+    pic.addEventListener('click', (e) => {
+        window.open("https://photos.app.goo.gl/RTEVkvdS5fLeFDD37")
+    })
+})
