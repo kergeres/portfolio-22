3a70951b3712aafd771a833e2556eee3e23f02b9
kergeres <55786735+kergeres@users.noreply.github.com>
2022-01-19 01:09:25 +0100

    adm bg, css effects

--- script/main.js
+++ script/main.js
@@ -15,14 +15,20 @@ document.querySelectorAll('.upper-container div, .lower-container div').forEach(
 
 function textAnimatoin() {
 
-    let top = document.querySelector(".text");
+    let top = document.querySelector(".text"); let bottom = document.querySelector(".zw");
+    let introPositionForB = bottom.getBoundingClientRect().top;
     let introPosition = top.getBoundingClientRect().top;
     let screenPosition = window.innerHeight / 1.7;
     if (introPosition < screenPosition) {
         top.classList.add("text-show");
     }
+    if (introPositionForB < screenPosition) {
+        bottom.classList.add("text-showb"); console.log("dddddddd");
+    }
+
 
 }
+
 
 
 window.addEventListener("scroll", function () {
